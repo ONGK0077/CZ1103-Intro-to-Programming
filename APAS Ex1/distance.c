6a0b4a3abd910b5d85b81fa918c5cbe9208089e7
ONGK0077 <82218018+ONGK0077@users.noreply.github.com>
2021-04-09 20:45:52 +0800

    Add files via upload

---
+++ APAS Ex1/distance.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+   /* Write your program code here */
+   float x1,x2,y1,y2,distance;
+
+   printf("Enter first point x1 y1:\n");
+   scanf("%f %f",&x1,&y1);
+   printf("Enter second point x2 y2:\n");
+   scanf("%f %f",&x2,&y2);
+
+   distance = sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
+
+   printf("The distance is %.2f\n",distance);
+   return 0;
+}
